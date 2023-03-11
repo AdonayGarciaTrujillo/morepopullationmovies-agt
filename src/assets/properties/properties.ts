973d2ce3d401929501fc80e0196de229e76fbce7
AdonayGarciaTrujillo <a3521110522@alumno.uttehuacan.edu.mx>
2023-03-10 22:55:33 -0600

    Primera

---
+++ src/assets/properties/properties.ts
@@ -0,0 +1,4 @@
+export const properties = {
+  logo:'https://previews.123rf.com/images/jethita/jethita1705/jethita170500022/79262967-computer-laptop-with-password-notification-and-lock-icon-on-concept-of-security.jpg'
+}
+
